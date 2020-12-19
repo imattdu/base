eceb24f2c24f6e4ae58261c94949bc9c3808051f
matt <matt17@qq.com>
2020-12-19 22:51:28 +0800

    添加日志

--- src/main/java/com/matt/project/base/controller/UserController.java
+++ src/main/java/com/matt/project/base/controller/UserController.java
@@ -2,6 +2,8 @@ package com.matt.project.base.controller;
 
 import com.matt.project.base.dataobject.UserDO;
 import com.matt.project.base.mapper.UserDOMapper;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,8 +20,13 @@ public class UserController {
     @Autowired
     private UserDOMapper userDOMapper;
 
+
+
     @GetMapping("/user/list")
     public List<UserDO> listUserDO(){
+
+        Logger logger = LoggerFactory.getLogger(UserController.class);
+        logger.info("hello");
 
         List<UserDO> userDOList = userDOMapper.listUserDO();
 
